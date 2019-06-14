3d91d146470feecaf8aa44d7b847ad652d5494d3
Thomas Duke <thmsduke@gmail.com>
2019-06-14 16:04:45 +1000

    Update folder structure with kedro framework

---
+++ src/twa_assignment/nodes/data_engineering.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+def preprocess_reviews(reviews: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the review data.
+
+        Args:
+            reviews: source data.
+        Returns:
+            Preprocessed data.
+
+    """
+
+    # Rename columns of data frame
+    reviews = reviews.rename(
+        columns={
+            "Unnamed: 0": "id",
+            "Clothing ID": "product_id",
+            "Age": "author_age",
+            "Title": "review_title",
+            "Review Text": "review_text",
+            "Rating": "star_rating",
+            "Recommended IND": "recommend_flag",
+            "Positive Feedback Count": "upvotes",
+            "Division Name": "product_category_division",
+            "Department Name": "product_category_department",
+            "Class Name": "product_category_class",
+        }
+    )
+
+    # Update review index
+    assert reviews["id"].is_unique, "Review identifier must be unique."
+    reviews = reviews.set_index("id")
+
+    # Lower case of category hierarchy
+    category_hierarchy = [
+        "product_category_division",
+        "product_category_department",
+        "product_category_class",
+    ]
+    reviews[category_hierarchy] = reviews[category_hierarchy].apply(
+        lambda x: x.str.lower(), axis=0
+    )
+
+    # Replace incorrect spelling of 'intimates'
+    reviews["product_category_division"] = reviews["product_category_division"].replace(
+        "initmates", "intimates"
+    )
+
+    return reviews
